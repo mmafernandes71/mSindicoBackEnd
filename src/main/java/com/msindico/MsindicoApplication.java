8f36ef74caa1d16ab02502c86aca1f83adcf5889
mmafernandes71 <mmafernandes71@gmail.com>
2018-07-16 18:41:43 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/msindico/MsindicoApplication.java
@@ -0,0 +1,12 @@
+package com.msindico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsindicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsindicoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/msindico/MsindicoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msindico;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsindicoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
